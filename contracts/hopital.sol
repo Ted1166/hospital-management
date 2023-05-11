2aea69847282f61d33583c9a5264a3b1484b2f32
Ted1166 <ogolated00@gmail.com>
2023-05-11 04:07:10 +0300

    hos

--- contracts/hopital.sol
+++ contracts/hopital.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 
 contract HospitalSystem {
